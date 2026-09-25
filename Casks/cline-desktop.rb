7e369672ea6ce0de15efbe3c47d3f5110cf2d424
Edgar Christian (Personal Access Token) <me@edgarcnp.dev>
2026-09-25 09:31:27 +0000

    chore(cask): update cline-desktop to 0.0.36

--- Casks/cline-desktop.rb
+++ Casks/cline-desktop.rb
@@ -1,6 +1,6 @@
 cask "cline-desktop" do
-  version "0.0.35"
-  sha256 "051eacbbf99f2a7ab75fa1edd1071995df1a9d99c8817ef4a5cc13bb83e812d7"
+  version "0.0.36"
+  sha256 "bc840e9d84d93b98db081f29de04694916ccafc3370fa9c1a3699e551fd2c36b"
 
   url "https://github.com/edgarcnp/homebrew-tap/releases/download/cline-desktop-v#{version}/cline-desktop-#{version}-x86_64.AppImage"
   name "Cline Desktop"
